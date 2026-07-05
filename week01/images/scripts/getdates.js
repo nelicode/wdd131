aa35db3266c08432c7703eeefe6739a004699760
nelicode <nelida11colque22@gmail.com>
2026-07-04 19:03:31 -0500

    commit

---
+++ week01/images/scripts/getdates.js
@@ -0,0 +1,6 @@
+// ===== CURRENT YEAR =====
+const currentYear = new Date().getFullYear();
+document.getElementById('currentyear').textContent = currentYear;
+
+// ===== LAST MODIFIED =====
+document.getElementById('lastModified').innerHTML = document.lastModified;
